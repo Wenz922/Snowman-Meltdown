f51e4c4c277b098b3854689e014f00e11e6b7580
Wenzheng Cai <wenzhengcai@Wenzhengs-MacBook-Air.local>
2025-08-23 01:10:43 +0200

    added replay option for player

--- snowman.py
+++ snowman.py
@@ -2,4 +2,9 @@ import game_logic
 
 
 if __name__ == "__main__":
-    game_logic.play_game()
+    while True:
+        game_logic.play_game()
+        replay_option = input("Play again (y/n)?: ").lower().strip()
+        if replay_option != "y":
+            print("Thanks for playing Snowman Meltdown!")
+            break
