3020ac8658ffdf4b1cf827c0dd7495fffab092aa
Wenzheng Cai <wenzhengcai@Wenzhengs-MacBook-Air.local>
2025-08-26 10:02:02 +0200

    updated the code to make it more efficiently and added docstring

--- game_logic.py
+++ game_logic.py
@@ -1,19 +1,20 @@
-import ascii_art
+from ascii_art import STAGES
 import random
 
 
-STAGES = ascii_art.STAGES
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
 
 
 def get_random_word():
     """Selects a random word from the list."""
-    return WORDS[random.randint(0, len(WORDS) - 1)]
+    return random.choice(WORDS)
 
 
 def display_game_state(mistakes, secret_word, guessed_letters):
-    # Display the snowman stage for the current number of mistakes.
+    '''
+    Display the snowman stage for the current number of mistakes.
+    '''
     print(STAGES[mistakes])
     # Build a display version of the secret word.
     display_word = ""
@@ -28,6 +29,9 @@ def display_game_state(mistakes, secret_word, guessed_letters):
 
 
 def play_game():
+    '''
+    Play the game loop until the game ends.
+    '''
     secret_word = get_random_word()
     guessed_letters = []
     mistakes = 0
